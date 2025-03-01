b393a3a937ef6d825fa337b8914cf26d5a593b7f
ahmed <asd@gmail.com>
2025-03-01 11:54:20 +0200

    add new variable(nVaildDays)

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,14 +8,18 @@
 
             Console.WriteLine("Please, Enter the Number Of Small Rooms: ");
             int nSmallRooms = Convert.ToInt32(Console.ReadLine());
+           // bool isConverted= int.TryParse(Console.ReadLine(),out nSmallRooms);
 
             Console.WriteLine("Please, Enter the Number Of large Rooms: ");
-            int nLargeRooms = Convert.ToInt32(Console.ReadLine());
+            int nLargeRooms = Convert.ToInt32(Console.ReadLine()); ;
+            //bool isConverted2 = int.TryParse(Console.ReadLine(),out nLargeRooms);
 
             int smallRoomsCost = nSmallRooms * 25;
             int largeRoomsCost = nLargeRooms * 35;
             int totalCost = smallRoomsCost + largeRoomsCost;
             double tax = totalCost * 0.06;
+            int nVaildDays = 30;
+
             Console.WriteLine("\n****************************************************\n");
             Console.WriteLine("-----\tEstimate for carpet cleaning service\t------\n");
             Console.WriteLine($"Price Per Small Rooms: {smallRoomsCost:C} ");
@@ -24,7 +28,7 @@
             Console.WriteLine($"Tax is : {(totalCost * 0.06) :C}");
             Console.WriteLine("\n--------------------------------------------------------\n");
             Console.WriteLine($"Total Estmate With Tax : {(totalCost + tax):C}");
-            Console.WriteLine("This estimate is vaild for 30 days");
+            Console.WriteLine($"This estimate is vaild for {nVaildDays} days");
             Console.WriteLine("\n****************************************************\n");
 
         }
