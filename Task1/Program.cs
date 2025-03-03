2c9c766bbe27efc8801b608c4cf0a1b8a9c2f987
ahmed <asd@gmail.com>
2025-03-03 14:36:15 +0200

    Put numbers in variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,30 +4,34 @@
     {
         static void Main(string[] args)
         {
+            int perSmallCharge = 25;
+            int perLargeCharge = 35;
+            const double tax = 0.06;
+            const int nVaildDays = 30;
+
             Console.WriteLine("\t--Welcom To Islam's Carpet Cleaning Service--\n");
 
             Console.WriteLine("Please, Enter the Number Of Small Rooms: ");
             int nSmallRooms = Convert.ToInt32(Console.ReadLine());
-           // bool isConverted= int.TryParse(Console.ReadLine(),out nSmallRooms);
+            int smallRoomsCost = nSmallRooms * perSmallCharge;
 
             Console.WriteLine("Please, Enter the Number Of large Rooms: ");
             int nLargeRooms = Convert.ToInt32(Console.ReadLine()); ;
-            //bool isConverted2 = int.TryParse(Console.ReadLine(),out nLargeRooms);
+            int largeRoomsCost = nLargeRooms * perLargeCharge;
 
-            int smallRoomsCost = nSmallRooms * 25;
-            int largeRoomsCost = nLargeRooms * 35;
+
+
             int totalCost = smallRoomsCost + largeRoomsCost;
-            double tax = totalCost * 0.06;
-            int nVaildDays = 30;
+            double calculateTax = totalCost * tax;
 
             Console.WriteLine("\n****************************************************\n");
             Console.WriteLine("-----\tEstimate for carpet cleaning service\t------\n");
             Console.WriteLine($"Price Per Small Rooms: {smallRoomsCost:C} ");
             Console.WriteLine($"Price Per Large Rooms: {largeRoomsCost:C} ");
             Console.WriteLine($"Total Cost before Tax: {totalCost:C} ");
-            Console.WriteLine($"Tax is : {(totalCost * 0.06) :C}");
+            Console.WriteLine($"Tax is : {(calculateTax) :C}");
             Console.WriteLine("\n--------------------------------------------------------\n");
-            Console.WriteLine($"Total Estmate With Tax : {(totalCost + tax):C}");
+            Console.WriteLine($"Total Estmate With Tax : {(totalCost + calculateTax):C}");
             Console.WriteLine($"This estimate is vaild for {nVaildDays} days");
             Console.WriteLine("\n****************************************************\n");
 
